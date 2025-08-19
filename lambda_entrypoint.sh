a883946670b3991149949252a6dcaeeefca0c0b5
Glenda L <lccbrokha@gmail.com>
2025-08-19 15:09:58 -0600

    remove-entry-point

--- lambda_entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ -z "${AWS_LAMBDA_RUNTIME_API}" ]; then
-  # Local con RIE
-  exec /usr/local/bin/aws-lambda-rie /usr/bin/python3 -m awslambdaric "$@"
-else
-  # En Lambda real
-  exec /usr/bin/python3 -m awslambdaric "$@"
-fi
